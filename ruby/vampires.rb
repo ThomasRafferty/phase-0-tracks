4233edf6a0391b4c4d60b8d341aea02aa653700d
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-06-24 13:16:50 -0400

    Make age to birthyear verification

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,13 +2,30 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "What year where you born?"
-birth_year = gets.chomp
+birth_year = gets.chomp.to_i
+age_based_on_birth_year = (2017 - birth_year)
 
 puts "Our company cafeteria is know for its amazing garlic bread.  Should we order you some? (yes/no)"
 garlic_bread_preferance = gets.chomp
 
 puts "Would you like to enroll in our company's health care plan?"
 healthcare_preferance = gets.chomp
+
+if age == age_based_on_birth_year
+  correct_age = true
+else
+  correct_age = false
+end
+
+
+
+valid_input = false
+
+until valid_input
+
+  if ()
+
+end
