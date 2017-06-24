c31f889d4db67ae2b20eb497f1868d22787930f8
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-06-24 03:08:16 -0400

    Create initial questions for canidate

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year where you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria is know for its amazing garlic bread.  Should we order you some? (yes/no)"
+garlic_bread_preferance = gets.chomp
+
+puts "Would you like to enroll in our company's health care plan?"
+healthcare_preferance = gets.chomp
