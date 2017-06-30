051380094c5cec8b00759c744f5b04e12eca3dbd
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-06-30 01:18:10 -0400

    Few more string method suspects identified

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -20,7 +20,7 @@ puts "Stop! You’re under arrest!".upcase
 "the usual".<< " suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
 "The case of the disappearing last letter".chop
@@ -30,16 +30,16 @@ puts "Stop! You’re under arrest!".upcase
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
-
+#Note: The (" ") is not need for this example, for there are no instances of repeated letters, yet if "boom!" were to be included in the string then the second "o" would be removed
 
 "z".getbyte(0)
 "z".ord
 "z".each_byte {|z| print z, ` ` }
 # => 122
 # (What is the significance of the number 122 in relation to the character z?)
-#The number 122 is the ASCII code for lowercase "z".  ASCII is the abbreviation from American Standard Code of Information Interchange.  ASCII is a encoding standard which allows for text can be communicated across all sorts of different devices and systems.
+# The number 122 is the ASCII code for lowercase "z".  ASCII is the abbreviation from American Standard Code of Information Interchange.  ASCII is a encoding standard which allows for text can be communicated across all sorts of different devices and systems.
 
 "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
