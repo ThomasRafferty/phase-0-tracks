32fbfff9580cdc8ca65c6631833e9d88274c1fef
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-01 17:08:45 -0400

    Write psuedocode and first nested structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,29 @@
+#Highway Nested Data Structures
+#Create Hash for each side of Highway (East/West) With that Create Lanes again using hashes.  Within Lanes create cars out of arrays of cars in which an array of descriptive information about the cars is nested
+
+highway = {
+  eastbound: {
+    lanes: {
+      fast_lane = [
+        [cars_east_fast = [
+          ['Mercedes-Benz', 'speed: 85 mph' 'Driver: Steve', 'Passengers: 4'],
+          ['Honda', 'speed: 67 mph', 'Driver: Mark', 'Passengers: 0'],
+          ['Mazda', 'speed: 65 mph', 'Driver: Mark', 'Passengers: 0']
+        ]
+      ],
+#      middle_lane: {
+#
+#
+#      },
+#      slow_lane: {
+#
+#      }
+#
+#    }
+#  }
+#
+#  westbound = {
+#
+#  }
+#
+#}#
