744c8a76b2aefc4f9f54ac0a46b93c4da7308f63
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-05 13:57:18 -0400

    Remove Lane hash and restructure car array

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -3,38 +3,33 @@
 
 highway = {
   eastbound: {
-      fast_lane: [
-        [cars_east_fast = [
-          ['Mercedes-Benz', 'speed: 85 mph' 'Driver: Steve', 'Passengers: 4'],
-          ['Honda', 'speed: 67 mph', 'Driver: Mark', 'Passengers: 0'],
-          ['Mazda', 'speed: 65 mph', 'Driver: Mark', 'Passengers: 0']
-        ]
-      ],
-      middle_lane: [
-        [cars_east_middle = [
-        ]
-      ],
-      slow_lane: [
-        [cars_east_low = [
-        ]
-      ]
+    cars_fast_lane = [
+        ['Driver: Steve' 'Mercedes-Benz', 'Speed: 85 mph', 'Passengers: 4'],
+        ['Driver: Stephanie', 'Honda', 'Speed: 67 mph', 'Passengers: 0'],
+        ['Driver: Jamal', 'Mazda', 'Speed: 65 mph', 'Passengers: 0']
+    ],
+    cars_middle_lane = [
+      ['Ford', 'Speed:64 mph', 'Driver: Jane', 'Passengers: 2'],
+      ['Honda', 'Speed: 60 mph', 'Driver: ']
+    ],
+    cars_slow_lane = [
     ]
   },
   westbound: {
-      fast_lane: [
-        [cars_west_fast = [
-          ['Mercedes-Benz', 'speed: 85 mph' 'Driver: Steve', 'Passengers: 4'],
-          ['Honda', 'speed: 67 mph', 'Driver: Mark', 'Passengers: 0'],
-          ['Mazda', 'speed: 65 mph', 'Driver: Mark', 'Passengers: 0']
-        ]
-      ],
-      middle_lane: [
-        [cars_west_medium = [
-        ]
-      ],
-      slow_lane: [
-        [cars_west_slow = [
-        ]
-      ]
+   fast_lane: [
+     cars_west_fast = [
+       ["Mercedse-Benz", "speed: 85 mph", "Driver: Steve", "Passengers: 4"],
+       ["Honda", "speed: 67 mph", "Driver: Mark", "Passengers: 0"],
+       ["Mazda", "speed: 65 mph", "Driver: Mark", "Passengers: 0"]
+     ]
+   ],
+   middle_lane: [
+     cars_west_medium = [
+     ]
+   ],
+   slow_lane: [
+     cars_west_slow = [
+     ]
+   ]
   }
 }
