97f17dd19bfe387061540b2e0ab556a49459b77b
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-06 15:29:21 -0400

    Add Car array info to lane arrays

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,37 +1,34 @@
 #Highway Nested Data Structures
 #Create Hash for each side of Highway (East/West) With that Create Lanes again using hashes.  Within Lanes create cars out of arrays of cars in which an array of descriptive information about the cars is nested
 
-highway = {
-  eastbound: {
-    cars_fast_lane = [
-        ['Driver: Steve' 'Mercedes-Benz', 'Speed: 85 mph', 'Passengers: 4'],
-        ['Driver: Stephanie', 'Honda', 'Speed: 67 mph', 'Passengers: 0'],
-        ['Driver: Jamal', 'Mazda', 'Speed: 65 mph', 'Passengers: 0']
+highway= {
+  eastbound_lanes: {
+    fast_lane: [
+        ['Driver: Steve ', 'Mercedes-Benz', 'Speed: 72 mph', 'Passengers: 4'],
+        ['Driver: Stephanie ', 'Honda', 'Speed: 67 mph', 'Passengers: 0'],
+        ['Driver: Jamal ', 'Mazda', 'Speed: 65 mph', 'Passengers: 1']
     ],
-    cars_middle_lane = [
-      ['Driver: Jane', 'Ford', 'Speed:64 mph', 'Passengers: 2'],
-      ['Driver: John' 'Honda', 'Speed: 60 mph', 'Passengers: 1'],
-      ['Driver: Brandon' 'Ford', 'Speed: 60 mph', 'Passengers: 1'],
+    middle_lane: [
+      ['Driver: Jane ', 'Ford', 'Speed:64 mph', 'Passengers: 2'],
+      ['Driver: John ' 'Honda', 'Speed: 60 mph', 'Passengers: 0'],
+      ['Driver: Brandon ' 'Ford', 'Speed: 60 mph', 'Passengers: 1'],
     ],
-    cars_slow_lane = [
+    slow_lane: [
       ['Driver: Grandma' 'Chevy', 'Speed: 25 mph', 'Passengers: 0'],
     ]
   },
-  westbound: {
+}
+  westbound_lanes: {
    fast_lane: [
-     cars_west_fast = [
-       ["Mercedse-Benz", "speed: 85 mph", "Driver: Steve", "Passengers: 4"],
-       ["Honda", "speed: 67 mph", "Driver: Mark", "Passengers: 0"],
-       ["Mazda", "speed: 65 mph", "Driver: Mark", "Passengers: 0"]
-     ]
-   ],
+      ['Driver: Grandpa', 'GMC', 'Speed: 85', 'Passengers: 1']
+      ['Driver: Max', 'Toyota', 'Speed: 70', 'Passengers: 0']
+    ],
    middle_lane: [
-     cars_west_medium = [
-     ]
-   ],
+    ],
    slow_lane: [
-     cars_west_slow = [
-     ]
+    ['Driver: Harry', 'Lexus', 'Speed: 65', 'Passengers: 3']
+    ['Driver: Phoebe', 'Honda', 'Speed: 55', 'Passengers: 0']
+    ['Driver: Elizabeth', 'Chevy', 'Speed: 54', 'Passengers: 1']
    ]
   }
 }
---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,58 @@
+#Release 0
+# We are taking a declared array and we are going construct a method that takes as an input an array and a possible integer in that array and returns its index number.
+
+arr = [42, 89, 23, 1]
+
+def search_array(input_array, input_search_integer)
+  index = 1
+  input_array.each do |x|
+    if input_search_integer == x
+      p index
+      break
+    end
+  index += 1
+  end
+  if index > input_array.length
+    puts "nil"
+  end
+end
+
+search_array(arr, 42)
+search_array(arr, 89)
+search_array(arr, 0)
+search_array(arr, -1)
+search_array(arr, 23)
+search_array(arr, 1)
+search_array(arr, 100)
+
+#Release 1
+#Construct an array that continues to increase the size of the array by adding new elements. This new element should be calculated by using the last two elements of the array.
+
+def fib(input_fibonacci)
+    fib_array = []
+  if input_fibonacci == 1
+    fib_array.push(0)
+  elsif input_fibonacci == 2
+    fib_array.push(0)
+    fib_array.push(1)
+  elsif input_fibonacci == 3
+    fib_array.push(0)
+    fib_array.push(1)
+    fib_array.push(1)
+  else
+    fib_array = [0, 1, 1]
+    until fib_array.length == input_fibonacci
+      fib_array.insert(-1, (fib_array[-1]+fib_array[-2]))
+    end
+  end
+  p fib_array
+end
+
+#Driver Code
+puts "How many numbers of the Fibonacci sequence would you like to see?"
+desired_index_fibonacci = gets.chomp.to_i
+
+fib(desired_index_fibonacci)
+
+# puts "Is the 100 Fibonacci number equal to  218922995834555169026?"
+# p fib(100).last == 218922995834555169026
