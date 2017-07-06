ef569c9e268777f7444d74e73a0bb5ab864f7d9e
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-06 15:32:26 -0400

    Debug code fixing commas

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,7 +1,7 @@
 #Highway Nested Data Structures
 #Create Hash for each side of Highway (East/West) With that Create Lanes again using hashes.  Within Lanes create cars out of arrays of cars in which an array of descriptive information about the cars is nested
 
-highway= {
+highway = {
   eastbound_lanes: {
     fast_lane: [
         ['Driver: Steve ', 'Mercedes-Benz', 'Speed: 72 mph', 'Passengers: 4'],
@@ -11,24 +11,25 @@ highway= {
     middle_lane: [
       ['Driver: Jane ', 'Ford', 'Speed:64 mph', 'Passengers: 2'],
       ['Driver: John ' 'Honda', 'Speed: 60 mph', 'Passengers: 0'],
-      ['Driver: Brandon ' 'Ford', 'Speed: 60 mph', 'Passengers: 1'],
+      ['Driver: Brandon ' 'Ford', 'Speed: 60 mph', 'Passengers: 1']
     ],
     slow_lane: [
       ['Driver: Grandma' 'Chevy', 'Speed: 25 mph', 'Passengers: 0'],
     ]
   },
-}
   westbound_lanes: {
    fast_lane: [
-      ['Driver: Grandpa', 'GMC', 'Speed: 85', 'Passengers: 1']
+      ['Driver: Grandpa', 'GMC', 'Speed: 85', 'Passengers: 1'],
       ['Driver: Max', 'Toyota', 'Speed: 70', 'Passengers: 0']
     ],
    middle_lane: [
     ],
    slow_lane: [
-    ['Driver: Harry', 'Lexus', 'Speed: 65', 'Passengers: 3']
-    ['Driver: Phoebe', 'Honda', 'Speed: 55', 'Passengers: 0']
+    ['Driver: Harry', 'Lexus', 'Speed: 65', 'Passengers: 3'],
+    ['Driver: Phoebe', 'Honda', 'Speed: 55', 'Passengers: 0'],
     ['Driver: Elizabeth', 'Chevy', 'Speed: 54', 'Passengers: 1']
    ]
   }
 }
+require pp
+p highway
