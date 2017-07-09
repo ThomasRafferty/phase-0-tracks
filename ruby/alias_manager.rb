1decd1af3a7fa243f4e47b73f268f2a581fbfd66
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-09 02:22:29 -0400

    Add downcase to input to method alias_manager

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,2 +1,12 @@
 #In order to address this problem, as the lease says we should break it into parts. First, downcase everything to limit the amount of possible charters which we have to possible match later.  From their use a matching loop to distinguish between vowels and consonants.
 
+def alias_manager(real_name)
+  real_name.downcase
+
+end
+
+#Driver
+puts "Please enter agent's real name to create an alias."
+input_name = gets.chomp
+
+alias_manager(input_name)
