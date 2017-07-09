7dfb8383dcbc99ae891a39453e12f61e295c85fa
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-09 02:20:22 -0400

    Initial commit and first attempt at psuedocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,2 @@
+#In order to address this problem, as the lease says we should break it into parts. First, downcase everything to limit the amount of possible charters which we have to possible match later.  From their use a matching loop to distinguish between vowels and consonants.
+
