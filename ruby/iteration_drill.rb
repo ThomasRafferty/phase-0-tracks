0b592d8728e385503e655a1f1df68852f529c31e
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-10 07:30:38 -0400

    Complete assignment

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -1,63 +1,54 @@
-# # Array Drills
-#
-# zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
-#                               "shotgun", "compass", "CB radio", "batteries"]
-#
-# # 1. Iterate through the zombie_apocalypse_supplies array using #each,
-# # printing each item in the array separated by an asterisk
-# # ----
-# zombie_apocalypse_supplies.each do |supply_item|
-#   puts "* #{supply_item}"
-# end
-#
-#
-# # 2. Create a method to see if a particular item (string) is in the
-# # zombie_apocalypse_supplies using #each.
-# # For instance: are boots in your list of supplies?
-# # ----
+# Array Drills
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+                              "shotgun", "compass", "CB radio", "batteries"]
+# 1. Iterate through the zombie_apocalypse_supplies array using #each,
+# printing each item in the array separated by an asterisk
+# ----
+zombie_apocalypse_supplies.each do |supply_item|
+  puts "* #{supply_item}"
+end
+# 2. Create a method to see if a particular item (string) is in the
+# zombie_apocalypse_supplies using #each.
+# For instance: are boots in your list of supplies?
+# ----
 
-## Could not figure out how to only have one outcome print :(
+# Could not figure out how to only have one outcome print :(
 
-# def item_deterctor(item_list_checked, possible_item)
-#   item_list_checked.each do |item_looking_for|
-#     if item_looking_for == possible_item
-#       puts "Item looking for was this item!"
-#     else
-#       puts "Item looking for was not this item."
-#     end
-#   end
-# end
-#
-# puts "What item would you like to check to see if in zombie apocalypse supplies?"
-# item_check = gets.chomp
-# item_deterctor(zombie_apocalypse_supplies, item_check)
-#
-# # 3. You can't carry too many things, you've only got room in your pack for 5.
-# # Remove items in your zombie_apocalypse_supplies in any way you'd like,
-# # leaving only 5, using #each.
-# # ----
-# until zombie_apocalypse_supplies.length == 5
-#   zombie_apocalypse_supplies.pop
-# end
-#
-# p zombie_apocalypse_supplies
-#
-# # 4. You found another survivor! This means you can combine your supplies.
-# # Create a new combined supplies list out of your zombie_apocalypse_supplies
-# # and their supplies below. You should get rid of any duplicate items.
-# # Find the built-in method that helps you accomplish this in the Ruby
-# # documentation for Arrays.
-# other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
-#                             "solar battery", "flashlight"]
-# other_survivor_supplies_index = 0
-# until other_survivor_supplies_index == other_survivor_supplies.length
-#   zombie_apocalypse_supplies.push(other_survivor_supplies[other_survivor_supplies_index])
-#   other_survivor_supplies_index += 1
-# end
-#
-# zombie_apocalypse_supplies.uniq!
-# p zombie_apocalypse_supplies
-# # ----
+def item_deterctor(item_list_checked, possible_item)
+  item_list_checked.each do |item_looking_for|
+    if item_looking_for == possible_item
+      puts "Item looking for was this item!"
+    else
+      puts "Item looking for was not this item."
+    end
+  end
+end
+puts "What item would you like to check to see if in zombie apocalypse supplies?"
+item_check = gets.chomp
+item_deterctor(zombie_apocalypse_supplies, item_check)
+# 3. You can't carry too many things, you've only got room in your pack for 5.
+# Remove items in your zombie_apocalypse_supplies in any way you'd like,
+# leaving only 5, using #each.
+# ----
+until zombie_apocalypse_supplies.length == 5
+  zombie_apocalypse_supplies.pop
+end
+p zombie_apocalypse_supplies
+# 4. You found another survivor! This means you can combine your supplies.
+# Create a new combined supplies list out of your zombie_apocalypse_supplies
+# and their supplies below. You should get rid of any duplicate items.
+# Find the built-in method that helps you accomplish this in the Ruby
+# documentation for Arrays.
+other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
+                            "solar battery", "flashlight"]
+other_survivor_supplies_index = 0
+until other_survivor_supplies_index == other_survivor_supplies.length
+  zombie_apocalypse_supplies.push(other_survivor_supplies[other_survivor_supplies_index])
+  other_survivor_supplies_index += 1
+end
+zombie_apocalypse_supplies.uniq!
+p zombie_apocalypse_supplies
+# ----
 
 # Hash Drills
 
@@ -127,32 +118,17 @@ p extinct_animals
         p possibly_extinct_animal
         if animal_look_up == possibly_extinct_animal
           p true
+          p "NOOOOOOOOOOOOOOOOOO!"
         else animal_look_up != possibly_extinct_animal
           p false
         end
-
       end
     end
 
 p animal_deterctor(extinct_animals, "Andean Cat")
 p animal_deterctor(extinct_animals, "Dodo")
 p animal_deterctor(extinct_animals, "Saiga Antelope")
-# puts "What animal extinction status would you like to look up? If finished enter done."
-# search_animal = gets.chomp
-# animal_deterctor(extinct_animals, search_animal)
 
-# break if possibly_extinct_animal == done
-
-# def item_deterctor(item_list_checked, possible_item)
-
-#   item_list_checked.each do |item_looking_for|
-#     if item_looking_for == possible_item
-#       puts "Item looking for was this item!"
-#     else
-#       puts "Item looking for was not this item."
-#     end
-#   end
-# end
 
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
@@ -160,3 +136,12 @@ p animal_deterctor(extinct_animals, "Saiga Antelope")
 # Find a Ruby Hash built-in method that helps you accomplish this or build
 # your own method using #each
 # ----
+
+p extinct_animals.delete("Passenger Pigeon")
+p extinct_animals
+
+
+#or
+
+#p extinct_animals.shift
+#p extinct_animals
