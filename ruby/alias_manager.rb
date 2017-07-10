229120f5f49e3f738d81b1d94a3ace49c2c52008
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-09 22:41:37 -0400

    Build consonant changer

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,7 +5,7 @@ agents_processed = gets.chomp.to_i
 entry_counter = 0
 until agents_processed == entry_counter
 
-     #Need to figure out how to get this array output
+#Need to figure out how to get this array output
 def array_preperation(input_name)
       input_name.downcase!
       name_array = real_name.split('')
@@ -33,22 +33,58 @@ def vowel_changer(vowel_input)
 end
 
 #Changing consonants in array to next consonants
-def consonant_changer(consonants_transformation)
+def consonant_changer(consonants_input)
   consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-   consonants_transformation.map! do |input_consonant|
-
-
+   consonants_input.map! do |input_character_c|
+      if input_character_c == consonants[0]
+        input_character_c = consonants[1]
+      elsif input_character_c == consonants[1]
+        input_character_c = consonants[2]
+      elsif input_character_c == consonants[2]
+        input_character_c = consonants[3]
+      elsif input_character_c == consonants[3]
+        input_character_c = consonants[4]
+      elsif input_character_c == consonants[4]
+        input_character_c = consonants[5]
+      elsif input_character_c == consonants[5]
+        input_character_c = consonants[6]
+      elsif input_character_c == consonants[6]
+        input_character_c = consonants[7]
+      elsif input_character_c == consonants[7]
+        input_character_c = consonants[8]
+      elsif input_character_c == consonants[8]
+        input_character_c = consonants[9]
+      elsif input_character_c == consonants[9]
+        input_character_c = consonants[10]
+      elsif input_character_c == consonants[10]
+        input_character_c = consonants[11]
+      elsif input_character_c == consonants[11]
+        input_character_c = consonants[12]
+      elsif input_character_c == consonants[12]
+        input_character_c = consonants[13]
+      elsif input_character_c == consonants[13]
+        input_character_c = consonants[14]
+      elsif input_character_c == consonants[14]
+        input_character_c = consonants[15]
+      elsif input_character_c == consonants[15]
+        input_character_c = consonants[16]
+      elsif input_character_c == consonants[16]
+        input_character_c = consonants[17]
+      elsif input_character_c == consonants[17]
+        input_character_c = consonants[18]
+      elsif input_character_c == consonants[18]
+        input_character_c = consonants[19]
+      elsif input_character_c == consonants[19]
+        input_character_c = consonants[20]
+      elsif input_character_c == consonants[20]
+        input_character_c = consonants[0]
+      else
+        input_character_c
+      end
+    end
 end
 
 
-
-      #Changing consonants in array to next consonants
-      consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-      name_array.map! do |input_consonant|
-        if input_consonant == c
-   end
-        # elsif input_character == character
-        #    input_character = character[+1]
 input_name = "Thomas Rafferty"
 alias_manager(input_name)
 #  name_array.map! { |name_array| name_array.next}
