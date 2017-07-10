c2b6e63cfb09b49f62610b960b1a180e8c65d787
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-10 05:59:44 -0400

    Complete Hash Q1 and Q2

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -1,60 +1,60 @@
-# Array Drills
-
-zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
-                              "shotgun", "compass", "CB radio", "batteries"]
-
-# 1. Iterate through the zombie_apocalypse_supplies array using #each,
-# printing each item in the array separated by an asterisk
-# ----
-zombie_apocalypse_supplies.each do |supply_item|
-  puts "* #{supply_item}"
-end
-
-
-# 2. Create a method to see if a particular item (string) is in the
-# zombie_apocalypse_supplies using #each.
-# For instance: are boots in your list of supplies?
-# ----
-def item_deterctor(item_list_checked, possible_item)
-  item_list_checked.each do |item_looking_for|
-    if item_looking_for == possible_item
-      puts "Item looking for was this item!"
-    else
-      puts "Item looking for was not this item."
-    end
-  end
-end
-
-puts "What item would you like to check to see if in zombie apocalypse supplies?"
-item_check = gets.chomp
-item_deterctor(zombie_apocalypse_supplies, item_check)
-
-# 3. You can't carry too many things, you've only got room in your pack for 5.
-# Remove items in your zombie_apocalypse_supplies in any way you'd like,
-# leaving only 5, using #each.
-# ----
-until zombie_apocalypse_supplies.length == 5
-  zombie_apocalypse_supplies.pop
-end
-
-p zombie_apocalypse_supplies
-
-# 4. You found another survivor! This means you can combine your supplies.
-# Create a new combined supplies list out of your zombie_apocalypse_supplies
-# and their supplies below. You should get rid of any duplicate items.
-# Find the built-in method that helps you accomplish this in the Ruby
-# documentation for Arrays.
-other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
-                            "solar battery", "flashlight"]
-other_survivor_supplies_index = 0
-until other_survivor_supplies_index == other_survivor_supplies.length
-  zombie_apocalypse_supplies.push(other_survivor_supplies[other_survivor_supplies_index])
-  other_survivor_supplies_index += 1
-end
-
-zombie_apocalypse_supplies.uniq!
-p zombie_apocalypse_supplies
-# ----
+# # Array Drills
+#
+# zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+#                               "shotgun", "compass", "CB radio", "batteries"]
+#
+# # 1. Iterate through the zombie_apocalypse_supplies array using #each,
+# # printing each item in the array separated by an asterisk
+# # ----
+# zombie_apocalypse_supplies.each do |supply_item|
+#   puts "* #{supply_item}"
+# end
+#
+#
+# # 2. Create a method to see if a particular item (string) is in the
+# # zombie_apocalypse_supplies using #each.
+# # For instance: are boots in your list of supplies?
+# # ----
+# def item_deterctor(item_list_checked, possible_item)
+#   item_list_checked.each do |item_looking_for|
+#     if item_looking_for == possible_item
+#       puts "Item looking for was this item!"
+#     else
+#       puts "Item looking for was not this item."
+#     end
+#   end
+# end
+#
+# puts "What item would you like to check to see if in zombie apocalypse supplies?"
+# item_check = gets.chomp
+# item_deterctor(zombie_apocalypse_supplies, item_check)
+#
+# # 3. You can't carry too many things, you've only got room in your pack for 5.
+# # Remove items in your zombie_apocalypse_supplies in any way you'd like,
+# # leaving only 5, using #each.
+# # ----
+# until zombie_apocalypse_supplies.length == 5
+#   zombie_apocalypse_supplies.pop
+# end
+#
+# p zombie_apocalypse_supplies
+#
+# # 4. You found another survivor! This means you can combine your supplies.
+# # Create a new combined supplies list out of your zombie_apocalypse_supplies
+# # and their supplies below. You should get rid of any duplicate items.
+# # Find the built-in method that helps you accomplish this in the Ruby
+# # documentation for Arrays.
+# other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
+#                             "solar battery", "flashlight"]
+# other_survivor_supplies_index = 0
+# until other_survivor_supplies_index == other_survivor_supplies.length
+#   zombie_apocalypse_supplies.push(other_survivor_supplies[other_survivor_supplies_index])
+#   other_survivor_supplies_index += 1
+# end
+#
+# zombie_apocalypse_supplies.uniq!
+# p zombie_apocalypse_supplies
+# # ----
 
 # Hash Drills
 
@@ -72,9 +72,19 @@ extinct_animals = {
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
 
+extinct_animals.each do |animal, extinction_date|
+  puts "#{animal}-#{extinction_date}*"
+end
+
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000, using #each.
 # ----
+extinct_animals.each do # |animal, extinction_date|
+  extinct_animals.delete_if { |animal, extinction_date| extinction_date < 2000}
+end
+
+p extinct_animals
+
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
