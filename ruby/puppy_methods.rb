8177a4494699327cd9a4e6eeb29c0c00c83a3009
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-11 17:24:44 -0400

    Commit real release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -31,12 +31,46 @@ class Puppy
 
 end
 
+class Squirrel
+
+  def climb_tree
+    puts "*climbs tree*"
+  end
+
+  def hide_acorn(integer)
+    acorn_count = 0
+    until acorn_count == integer
+      puts "Hide acorn"
+      acorn_count += 1
+    end
+  end
+
+  def initialize
+    puts "Initializing squirrel instance"
+  end
+
+end
+
 
 #Driver Code ##########################################
 maggie = Puppy.new
+p maggie
+p maggie.class
 
 maggie.fetch("ball")
 maggie.speak(5)
 maggie.roll_over
 maggie.dog_years(2)
 maggie.sit
+
+squirrel_count = 0
+squirrel_home = []
+until squirrel_count == 50
+  squirrel_home << Squirrel.new
+  squirrel_count += 1
+end
+
+squirrel_home.each do |squirrel|
+  squirrel.climb_tree
+  squirrel.hide_acorn(2)
+end
