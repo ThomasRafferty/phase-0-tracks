32a5f561e49510cced63401a2eafe5eda9c1f4dc
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-11 15:53:41 -0400

    Complete Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,6 +5,25 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    woof_count = 0
+    until woof_count == integer
+      puts "Woof!"
+      woof_count += 1
+    end
+  end
+
+  def roll_over
+    puts "*roll over*"
+  end
+
+  def dog_years (integer)
+    p integer * 7
+  end
+
+  def sit
+    puts "*sit*"
+  end
 end
 
 
@@ -12,3 +31,7 @@ end
 maggie = Puppy.new
 
 maggie.fetch("ball")
+maggie.speak(5)
+maggie.roll_over
+maggie.dog_years(2)
+maggie.sit
