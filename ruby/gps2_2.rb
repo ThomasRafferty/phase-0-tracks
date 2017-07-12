3cb25d052370fbcda3c91e8963fec0de501db761
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-12 00:11:50 -0400

    Comlete Release 4:Reflect

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -73,3 +73,10 @@ list_adder(groceries_2, "Ice Cream", 4)
 update_quantity(groceries_2, "Ice Cream", 1)
 list_remover(groceries_2, "Lemonade")
 pretty_up(groceries_2)
+
+#Pseudocode is definitely something I need to work on to make my code accessible and to clarify my thinking.  One thing I learned from this challenge is to think more out input and output when I am pseudocoding.
+#Arrays are nice for lists, particularly when there is no need to have additional information connected to the list items.  This challenged lent itself more to hashes for we wanted to attach quantities to the items on our grocery list.  So instead of having an array with ["ice cream", "pizza", "pizza", ect... ] we can have a hash with ["ice cream"=>1, "pizza"=>2, ect...]
+#A method in ruby always returns one object.  That object can be nil.
+#Methods can take all sorts of arguments including strings, integers, objects (which includes the previously listed), and even the output of another method.
+#You can pass information between methods by executing a method as the argument in another method.
+#This challenge reinforced working with hashes and adjusting their values.  I think everything this challenge currently makes sense to me, but there is a real possibility that I do yet know what I do not know.
