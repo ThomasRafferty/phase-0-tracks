3b14336815c8c8a8b9c4e6d57ae1a3f30b3f1185
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-12 18:32:44 -0400

    Complete release 0, create Santa class with some methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,22 @@
 class Santa
 
   def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies (cookie_type)
+    puts "That was a good #{cookie_type}"
+  end
+
+  def initialize
+  end
+
+end
+
+
+#Driver Code#####################################
+#Yes he is real and attends SantaCon with no need for a disguise.
+real_santa = Santa.new
+
+real_santa.speak
+real_santa.eat_milk_and_cookies("chocolate chip")
