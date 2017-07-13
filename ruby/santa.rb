942db4792ec668d1a15ec628d6ea457397e2b904
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-13 16:46:00 -0400

    Progress on santa creator

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -33,19 +33,40 @@ class Santa
   end
 end
 
-
-#Driver Code#####################################
 santas = []
-genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "cis man", "trans female", "male", "gender questioning", "female"]
-ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Iraqi", "white", "African American", "Native American", "Chinese"]
-genders.length.times do |i|
-  santas << Santa.new(genders[i], ethnicities[i])
-  puts "Hurray, SantaCon has grown! There are now #{santas.length} santas at SantaCon.  The more the merrier!"
+example_genders = ["agender", "female", "male", "bigender", "gender fluid", "cis man", "cis female", "trans female", "trans male", "gender questioning",  "N/A"]
+example_ethnicities = ["Native American", "Chinese", "Indian", "Iraqi", "Russian", "English", "Ghanaian", "Mexican", "Brazilian", "Canadian", "Japanese-African","African American", "Latino", "black", "white", "black", "Latino", "white", "Mystical Creature (unicorn)", "prefer not to say"]
+
+def santa_creator2(number_of_santas, example_genders,  example_ethnicities)
+  santas_at_santacon = 0
+  until santas_at_santaCon = number_of_santas
+    new_santa = Santa.new
+    new_santa.gender = example_genders.sample
+    new_santa.ethnicity = example_ethnicities.sample
+    p new_santa
+  end
+  santas_at_santacon += 1
 end
 
-p santas[0].get_mad_at("Dasher")
-p santas[6].celebrate_birthday
-p santas[0].age
-p santas[1].ethnicity
-p santas[11].gender = ("trans male")
 
+#Driver Code#####################################
+p santa_creator2(12, example_genders, example_ethnicities)
+# p santas
+
+# genders.length.times do |i|
+#   santas << Santa.new(genders[i], ethnicities[i])
+#   puts "Hurray, SantaCon has grown to #{santas.length} # santas!  The more the merrier!"
+# end
+#
+# p santas[0].get_mad_at("Dasher")
+# p santas[6].celebrate_birthday
+# p santas[0].age
+# p santas[1].ethnicity
+# p santas[11].gender = ("trans male")
+
+#Opps, first build Santas into an array, kept for reference.
+    # def santa_creator(input_array, number_of_santas,    example_genders,  example_ethnicities)
+    #   until input_array.length == number_of_santas
+    #       input_array << Santa.new(example_genders.sample, #    example_ethnicities.sample)
+    #         puts "Hurray, SantaCon has grown to {santas.length} # santas!  The more the merrier!"
+    #   end
