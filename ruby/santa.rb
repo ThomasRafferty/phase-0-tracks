6034c1e247bf72bcfecf3f59d2fb145e1193697a
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-14 01:15:20 -0400

    Complete and debug loop, as well as add interation to showcase other methods created to work with Santa Class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,4 @@
+#Create a Santa class and adjust as the releases instruct creating methods for the class as well as utilizing setter and getter methods as well as there basically shorthand attribute reader, and attribute accessor.
 class Santa
   attr_reader :age, :ethnicity
   attr_accessor :gender
@@ -9,8 +10,6 @@ class Santa
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
-  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -19,8 +18,12 @@ class Santa
     puts "That was a good #{cookie_type}"
   end
 
+  def age
+    @age = rand(140)
+  end
+
   def celebrate_birthday
-    @age = (age+1)
+    @age = (@age+1)
   end
 
   def get_mad_at(reindeer_name)
@@ -31,13 +34,8 @@ class Santa
       end
     end
   end
-
-  def age
-    @age = rand(140)
-  end
 end
 
-santas = []
 example_genders = ["agender", "female", "male", "bigender", "gender fluid", "cis man", "cis female", "trans female", "trans male", "gender questioning",  "N/A"]
 example_ethnicities = ["Native American", "Chinese", "Indian", "Iraqi", "Russian", "English", "Ghanaian", "Mexican", "Brazilian", "Canadian", "Japanese-African","African American", "Latino", "black", "white", "black", "Latino", "white", "Mystical Creature (unicorn)", "prefer not to say"]
 
@@ -51,23 +49,24 @@ def santa_creator(number_of_santas, gender, ethnicity)
   end
 end
 
-#Driver Code#####################################
+#Driver Code##############################################
+
 santa_creator(100, example_genders, example_ethnicities)
 
+#Yes, Santa is real and loves being able to attend SantaCon without a disguise.
+p ""
+real_santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+real_santa.speak
+real_santa.get_mad_at("Dasher")
+p real_santa.age
+p "SantaCon happens to be Santa's birthday.  They turned #{real_santa.celebrate_birthday}, happy birthday Santa!"
+p real_santa
 
-#Could not get these commands to work accept
-#p new_santa.get_mad_at("Dasher")
-# p santas[6].celebrate_birthday
-# p santas[0].age
-# p santas[1].ethnicity
-# p santas[11].gender = ("trans male")
-
-# p santas
 
 #Opps, first build Santas into an array, kept for reference.
-#    def santa_creator2(input_array, number_of_santas,    #example_genders,  example_ethnicities)
-#       until input_array.length == number_of_santas
-#           input_array << Santa.new(example_genders.sample,     #example_ethnicities.sample)
-#             puts "Hurray, SantaCon has grown to {santas.length} ## santas!  The more the merrier!"
-#      end
-#    end
+  #  def santa_creator2(input_array, number_of_santas,    example_genders,  example_ethnicities)
+  #     until input_array.length == number_of_santas
+  #         input_array << Santa.new(example_genders.sample,     example_ethnicities.sample)
+  #          puts "Hurray, SantaCon has grown to {santas.length}  santas!  The more the merrier!"
+  #     end
+  #   end
