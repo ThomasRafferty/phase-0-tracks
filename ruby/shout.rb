683abc4510e1277b1e4975e560752ab6ec7ec23b
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-14 02:25:58 -0400

    Create module, yet to create content

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,17 +1,51 @@
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+  def yell
 
-  def self.yell_happily(words)
-    words + "Eureka!!!!!!"
+class Umpire
+  include Shout
+end
+
+class
+
+module Flight
+  def take_off(altitude)
+    puts "Taking off and ascending until reaching #{altitude} ..."
   end
 end
 
+class Bird
+  include Flight
+end
+
+class Plane
+  include Flight
+end
+
+#Driver Code################################################
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
+
 #Driver Code##################################################
 
-p Shout.yell_happily("E = MC squared. ")
-p Shout.yell_angrily("Noooooo ")
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#
+#   def self.yell_happily(words)
+#     words + "Eureka!!!!!!"
+#   end
+# end
+#Old Driver Code######################
+# p Shout.yell_happily("E = MC squared. ")
+# p Shout.yell_angrily("Noooooo ")
+
+
+
+
 
 
 
