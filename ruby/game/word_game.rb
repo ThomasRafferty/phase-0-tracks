5b114f12cc7025bc95781a5ad1baf9195b01c3e5
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-17 02:51:03 -0400

    New aproach to letter hider

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,30 +1,32 @@
 #Create a game which has 2 players.  The first player enters a word (or phrase) which will hence be called the desired word.  The point of the game is for the second player to guess the desired word by entering possible letters.  The number of guesses given to the second player should be based on the length of the desired word (possible a third or half the length, which can be better determined upon testing.  Repeat guesses should not count against player 2.  As player 2 makes guess the amount of the desired word they have discovered, using their guesses, should be displayed changing from an unknown symbol to seeing the correct letters and these placement in the desired word.  It player 2 runs out of guesses before discovering the desired word then player 2 loses, and player 1 wins, causing a message taunting player 2 to print.  If player 2 completes the word in less than or equal to the available guess then player 2 wins the game and a message congratulating them will print.
 
-class Word_game
-  attr_reader :guess_count, :is_over
-  attr_accessor :game_word
+ class Word_game
+  attr_reader :guess_count, :game_word, :guess_array
+  attr_accessor :guess_letter_array
 
   def initialize(player_1_input)
     @game_word = player_1_input
     @guess_count = 0
-    @is_over = false
+    @guess_letter_array = []
+    #might us loop @is_over = false
   end
 
-  game_length = (@game_word.length)/2
-
-
-  def word_disuiser(word)
-    disguised_array = word.split("")
-   disguised_array.map! do |letter|
-     if letter == " "
-       letter = " "
-     else
-       letter = "-"
-     end
-   end
-   p disguised_array.join("")
+  def add_guess_letter (guess)
+    @guess_letter = guess
+    @guess_letter_array << (@guess_letter)
   end
 
+  def already_guess_letter
+    @guess_letter_array.each do
+      @guess_letter_array == @guess_letter
+    end
+  end
+end
 
 
-#Until game_length = guess_count then is_over = false
+
+
+game = Word_game.new("cat fish")
+
+p game.add_guess_letter("c")
+p game.already_guess_letter
