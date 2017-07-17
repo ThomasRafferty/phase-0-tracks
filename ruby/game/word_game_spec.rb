ba328d7eb48e568ac001fe34f825e818ffdc5eb8
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-17 08:07:26 -0400

    Add letter_guesser to spec

--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -7,4 +7,11 @@ describe Word_game do
     expect(game.add_guess_letter("a")).to eq ["a"]
   end
 
+  it "takes a letter and sees if it is the word game array" do expect(game.letter_guesser("c")).to eq ["c","-","-"]
+  end
+
+
 end
+
+#  it "takes input word and turns it into dash marks" do expect(game.word_disguiser(["c","a","t"]).to eq "---"
+#end
