ff033abb4ab3fe7d28b1ef1f268c43ce49bc097f
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-17 22:46:01 -0400

    Complete release 3

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -9,20 +9,21 @@
 require_relative 'state_data'
 
 class VirusPredictor
-
+#Initializing a new instance of the class of a new VirusPredictor.  It is taking in information about the state (state_of_origin, population_density and population). It is then creating instance variables using this input data.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+#It calls the two other methods described below.
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
   private
-
+#Method calculates the impact of the influence will likely have on the population within a given state.  It makes this calculation using conditional statements based on calculation density. Then prints that data for that given state.
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -41,6 +42,7 @@ class VirusPredictor
 
   end
 
+# This method also uses the population density to calculate the speed at which the disease will spread within the state.  It again uses conditional statements to determine the speed using population density then prints the calculated data.
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
