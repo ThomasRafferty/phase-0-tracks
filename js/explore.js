aba1fc6fe290e7459ad28f2f3e61aff6c9aacbb6
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-24 12:21:30 -0400

    Unfinished 7.1 confuses

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,8 @@
-// In JavaScript I am going to write a function that will reverse strings using the example hello.  First we will declare a variable set to a sting which we can input into the function I am building.  In the function we will need three statements.  First, we will initialize the counter.  Next we will set a condition that will determine the length of the of the loop (Is this needed on a string, like it was on an integer?).  THh
+// In JavaScript I am going to write a function that will reverse strings using the example hello.  First we will declare a variable set to a sting which we can input into the function I am building.  In the function we will need three statements.  First, we will initialize the counter.  Next we will set a condition that will determine the length of the of the loop (Is this needed on a string, like it was on an integer?).
+var str = "Reverse me"
+
+
+// I am just not getting this write now, not sure how the i work in the loop in order to work backwards.
+for (var i = 0; i < str.length; i++) {
+  console.log(str[i])
+}
