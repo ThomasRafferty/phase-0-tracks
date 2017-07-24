ce92967fca514730b761ea4afa0b9b96383823ff
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-07-24 12:41:13 -0400

    Poor attempt at 7.3

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+// I got so into studying for ruby I put JS off and completely do not get it, yet have also not put in the time.  Will spend this week hard at work figuring out where I'm at, sorry for this poor attempt at an assignment.
+
+
+array = ["long phrase","longest phrase","longer phrase"]
+
+
+//Loop through strings in the array finding the longest string, possible update as we go through
+function return_longest(input_array);
+  for (var i = 0, i < array.length, i++){
+    if input_array[i].length > input_array[i+1].length
+    longest_string = input_array[i]
+    p longest_string
+  }
