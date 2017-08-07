158c93fec745cd2036c4a6571d2237cf747b30f1
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-08-06 21:51:25 -0400

    Make unsuccesful  attempt at release 2.

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -6,6 +6,7 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
+# school = SQLite3::Database.new("campuses.db")
 
 # show students on the home page
 get '/' do
@@ -28,4 +29,19 @@ get '/ages' do
   @students = db.execute("SELECT * FROM students")
   erb :ages
 end
+
+
+#Could not figure out release 2.  Could not access the campuses.db which we created.
+
+# get '/campus' do
+#   "Hello Word"
+#   @campus = school.execute("SELECT * FROM locations")
+#   # erb :campus
+# end
+
+# post '/campus' do
+#   @campus = school.execute
+#   school.execute("INSERT INTO locations (name, good_food) VALUES (?,?)", [params['name'], params['good_food'])
+#   erb :campus
+# end
 # add static resources
